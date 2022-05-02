e926b2c5f4cb3f445a4683a33a7550406ac0203e
Ryan Toews <rtoews11@my.bcit.ca>
2022-05-02 14:46:15 -0700

    Ryan - Split name into first and last names, add 2 sample tuples

--- userDatabase.sql
+++ userDatabase.sql
@@ -2,7 +2,8 @@ CREATE DATABASE IF NOT EXISTS members;
 use members;
 CREATE TABLE IF NOT EXISTS user (
   ID int NOT NULL AUTO_INCREMENT,
-  name varchar(50),
+  firstName varchar(25),
+  lastName varchar(25),
   email varchar(25),
   password varchar(25),
   city varchar(25),
@@ -10,3 +11,6 @@ CREATE TABLE IF NOT EXISTS user (
   PRIMARY KEY (ID)
   );
 
+INSERT INTO user 
+VALUES ("1", "Nacho", "Varga", "nachov@bcs.com", "pass0987", "North Delta", "1981-03-25"),
+  ("2", "Saul", "Goodman", "saulg@bcs.com", "pass0987", "North Delta", "1963-07-14")
