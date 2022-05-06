f3a1ccbb2170c6914a303423acf0a59f89ae223f
Ryan Toews <rtoews11@my.bcit.ca>
2022-05-06 13:26:27 -0700

    Ryan - added a script for identifying the database

---
+++ scripts/database.js
@@ -0,0 +1,9 @@
+module.exports = {
+    'connection': {
+        'host': 'localhost',
+        'user': 'root',
+        'password': 'password'
+    },
+	'database': 'members',
+    'users_table': 'user'
+};
