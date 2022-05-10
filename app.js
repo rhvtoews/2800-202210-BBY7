b92a99d34c688446c722f6cd245f60b06a783f29
Ryan Toews <rtoews11@my.bcit.ca>
2022-05-10 11:10:33 -0700

    Ryan - rewrote login and session JS

--- app.js
+++ app.js
@@ -1,52 +1,139 @@
+"use strict";
 const express = require('express');
 const session = require('express-session');
-const cookieParser = require('cookie-parser');
 const app = express();
-const port = 3000;
-const day = 1000 * 60 * 60 * 24;
+const router = express.Router();
+const bodyParser = require('body-parser');
+const mysql = require('mysql2');
+const MySQLStore = require('express-mysql-session')(session);
+const port = 8000;
 
-app.use(session({
-  secret: "secret234234key234324token690848064",
-  saveUninitialized: true,
-  cookie: { invalidTime : day },
-  resave: false
-}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(__dirname));
-app.use(cookieParser());
+app.use(express.static(__dirname + '/'));
 
-const username = 'testUser'
-const password = 'test1234'
+// const POST = process.env.PORT || 8000;
 
-var thisSession;
 
-app.get('/', (req, res) => {
-  thisSession = req.thisSession;
-  if (thisSession.userid) {
-    res.send("Welcome <a href=\'/logout'>Click to Logout</a>");
+//dummy account
+const userData = {
+  fullname: "Ryan Toews",
+  username: "ryant",
+  password: 12345
+}
+
+var options = {
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: '',
+  database: 'BBY7_members'
+}
+
+var sessionConnection = mysql.createConnection(options);
+var sessionData = new MySQLStore({
+  expiration: 10800000,
+  createDatabaseTable: true,
+  schema:{
+    tableName: 'sessiontbl',
+    columnNames:{
+      session_id: 'session_id',
+      expires: 'expires',
+      data: 'data'
+    }
+  }
+}, sessionData)
+
+app.use(session ({
+  key: 'keyin',
+  secret: 'secret0982348934',
+  strore: sessionData,
+  resave: false,
+  saveUninitialized: true
+}))
+
+app.use('/login', function(req, res) {
+  const { username , password } = req.body
+  if (username != userData.username || password != userData.password) {
+    return res.status(401).json({
+      error: true,
+      message: "Invalid Username or Password"
+    })
   } else {
-    res.sendFile('/index.html',{root: __dirname})
+    req.session.userinfo = userData.fullname
+    res.send("Login successful")
   }
-});
+})
 
-app.post('/user', (req,res) => {
-  if(req.body.username == username && req.body.password == password){
-      thisSession=req.thisSession;
-      thisSession.userid=req.body.username;
-      console.log(req.thisSession)
-      res.send("Welcome <a href=\'/logout'>Click to Logout</a>");
+app.use('/logout', function(req,res){
+  req.session.destroy(function(err){
+      if(!err){
+          res.send("Logged Out")
+      }
+  })
+})
+
+app.use('/', function(req, res) {
+  if(req.session.userinfo) {
+    res.send("Welcome" + req.session.userinfo)
+  } else {
+    res.send("Please log in")
   }
-  else{
-      res.send('Invalid username or password');
-  }
-});
+})
 
-app.get('/logout', (req, res) => {
-  req.thisSession.destroy();
-  res.redirect('/');
-});
 
-app.listen(port, () => console.log('Server Running on ' + port));
+
+// var thisSession;
+
+// router.get('/', (req, res) => {
+//   thisSession = req.session;
+//   if (thisSession.username) {
+//     return res.redirect('/admin');
+//   }
+//   res.sendFile('index.html')
+// })
+
+// app.use(session({
+//   secret: "secret234234key234324token690848064",
+//   saveUninitialized: true,
+//   resave: false
+// }));
+
+// router.post('/login', (req, res) => {
+//   thisSession = req.session;
+//   thisSession.username = req.body.username;
+//   res.end('done');
+// })
+
+// app.get('/', function(req, res){
+//   thisSession =  req.session;
+//   thisSession.username;
+//   thisSession.password;
+// });
+
+// router.get('/admin', (req, res) => {
+//   thisSession = req.session;
+//   if (thisSession.username) {
+//     res.write('<h1>Hello ${thisSession.username} <h1><br>');
+//     res.end('+', 'Logout');
+//   } else {
+//     res.write('Please login first.');
+//   }
+// });
+
+// router.get('/logout', (req, res) => {
+//   req.session.destroy((err) => {
+//     if (err) {
+//       return console.log(err);
+//     }
+//     res.redirect('/');
+//   });
+// });
+
+// app.use('/', router);
+
+app.listen(process.env.PORT || 8000, () => {
+  console.log('App started on port ' + port);
+});
 
--- userDatabase.sql
+++ userDatabase.sql
@@ -1,11 +1,12 @@
-CREATE DATABASE IF NOT EXISTS members;
-use members;
+CREATE DATABASE IF NOT EXISTS BBY7_members;
+use BBY7_members;
 
 
-CREATE TABLE IF NOT EXISTS user (
+CREATE TABLE IF NOT EXISTS BBY7_user (
   ID int NOT NULL AUTO_INCREMENT,
   firstName varchar(25),
   lastName varchar(25),
+  username varchar(25),
   email varchar(25),
   password varchar(25),
   city varchar(25),
@@ -15,32 +16,32 @@ CREATE TABLE IF NOT EXISTS user (
   );
 
 
--- CREATE TABLE IF NOT EXISTS plant (
+-- CREATE TABLE IF NOT EXISTS BBY7_plant (
 --   plantName varchar(50) NOT NULL,
 --   plantType varchar(25),
 --   soilType varchar(25),
 --   region varchar(25),
 --   period int,
 --   PRIMARY KEY (plantName),
---   FOREIGN KEY (region) REFERENCES user(city)
+--   FOREIGN KEY (region) REFERENCES BB7_user(city)
 -- );
 
--- CREATE TABLE IF NOT EXISTS toolList (
+-- CREATE TABLE IF NOT EXISTS BB7_toolList (
 --   pName varchar(50) NOT NULL,
 --   tool varchar(25),
 --   PRIMARY KEY (pName),
---   FOREIGN KEY (pName) REFERENCES plant(plantName)
+--   FOREIGN KEY (pName) REFERENCES BB7_plant(plantName)
 -- );
 
 
-INSERT INTO user 
-VALUES ("1", "Nacho", "Varga", "nachov@bcs.com", "pass0987", "North Delta", "1981-03-25", TRUE),
-  ("2", "Saul", "Goodman", "saulg@bcs.com", "pass1234", "Saskatoon", "1963-07-14", TRUE),
-  ("3", "Dexter", "Morgan", "dexterm@mpd.com", "pass0987", "Burnaby", "1974-04-22", FALSE),
-  ("4", "Rita", "Bennett", "ritab@gmail.com", "pass1234", "Vancouver", "1975-03-16", FALSE),
-  ("5", "Naomi", "Nagata", "naomin@bl.com", "pass0987", "Calgary", "1990-10-23", FALSE),
-  ("6", "Alex", "Kamal", "alexk@mcrn.com", "pass1234", "Edmonton", "1963-07-14", FALSE),
-  ("7", "Amos", "Burton", "amosb@ba.com", "pass0987", "Toronto", "1988-06-07", FALSE),
-  ("8", "Camina", "Drummer", "caminad@bl.com", "pass1234", "Ottawa", "1985-11-18", FALSE),
-  ("9", "Harry", "Bosch", "harryb@lapd.com", "pass0987", "Winnipeg", "1967-03-09", FALSE),
-  ("10", "Jerry", "Edgar", "jerrye@lapd.com", "pass1234", "Red Deer", "1985-09-13", FALSE);
+INSERT INTO BBY7_user 
+VALUES ("1", "Nacho", "Varga", "nvarga", "nachov@bcs.com", "pass0987", "North Delta", "1981-03-25", TRUE),
+  ("2", "Saul", "Goodman", "sgoodman", "saulg@bcs.com", "pass1234", "Saskatoon", "1963-07-14", TRUE),
+  ("3", "Dexter", "Morgan", "dmorgan", "dexterm@mpd.com", "pass0987", "Burnaby", "1974-04-22", FALSE),
+  ("4", "Rita", "Bennett", "rbennett", "ritab@gmail.com", "pass1234", "Vancouver", "1975-03-16", FALSE),
+  ("5", "Naomi", "Nagata", "nnagata", "naomin@bl.com", "pass0987", "Calgary", "1990-10-23", FALSE),
+  ("6", "Alex", "Kamal", "akamal", "alexk@mcrn.com", "pass1234", "Edmonton", "1963-07-14", FALSE),
+  ("7", "Amos", "Burton", "aburton", "amosb@ba.com", "pass0987", "Toronto", "1988-06-07", FALSE),
+  ("8", "Camina", "Drummer", "cdrummer", "caminad@bl.com", "pass1234", "Ottawa", "1985-11-18", FALSE),
+  ("9", "Harry", "Bosch", "hbosch", "harryb@lapd.com", "pass0987", "Winnipeg", "1967-03-09", FALSE),
+  ("10", "Jerry", "Edgar", "jedgar", "jerrye@lapd.com", "pass1234", "Red Deer", "1985-09-13", FALSE);
