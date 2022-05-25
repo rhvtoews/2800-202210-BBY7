cfb9587c9dbf32c6ffe2b288e9d5217d66f90494
rhvtoews <ryantoews@gmail.com>
2022-05-24 21:04:27 -0700

    Ryan - added plant counter functionality to profile page

--- scripts/profile.js
+++ scripts/profile.js
@@ -12,10 +12,13 @@ function placeUser(data) {
   const fullname = document.getElementById('fullname');
   const email = document.getElementById('email');
   const photo = document.getElementById('photo');
+  const planted = document.getElementById('planted');
+  const region = document.getElementById('region');
 
   fullname.innerHTML = data[0].fullname;
   email.innerHTML = data[0].email;
   photo.src = ("./Image/profile/" + data[0].image);
-
+  planted.innerHTML = data[0].plantCounter;
+  region.innerHTML = data[0].region;
 
 }
